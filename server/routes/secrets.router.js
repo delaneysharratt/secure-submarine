9ffe8b133e3b08fa455ce62cbd622e072994c750
Laney McKee <laneymckeephotography@gmail.com>
2019-09-04 14:59:02 -0500

    login access restriction setup

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,19 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    if (req.user.clearance_level >= 0) {
+      console.log('Cleared for access!');
+      pool
+        .query('SELECT * FROM "secret";')
         .then(results => res.send(results.rows))
         .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
+          console.log('Error making SELECT for secrets:', error);
+          res.sendStatus(500);
         });
+    }
+  }
 });
 
 module.exports = router;
